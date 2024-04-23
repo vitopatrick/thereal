dc45cb28acf0aa7b36604f431a2bd72264379681
warlock <fecypher@gmail.com>
2024-04-23 20:32:42 +0100

    fixed

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,22 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+import {
+  addressFour,
+  addressThree,
+  addressTwo,
+  addresses,
+} from "../lib/wallet-address";
+
+export function getAddress(address_number: number | any) {
+  if (address_number === 1) {
+    return addresses;
+  } else if (address_number === 2) {
+    return addressTwo;
+  } else if (address_number === 3) {
+    return addressThree;
+  } else if (address_number === 4) {
+    return addressFour;
+  }
+}
